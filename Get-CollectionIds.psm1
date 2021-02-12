2898a795decb4dc7318baa08e373a33b2dd72a57
Neko Boi Nick <thakyz@outlook.com>
2021-02-12 10:27:15 -0800

    Create Get-CollectionIds.psm1

---
+++ Get-CollectionIds.psm1
@@ -0,0 +1,42 @@
+function Get-CollectionIds {
+param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [string]
+    $Id = "",
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Output = ""
+  )
+
+  $WorkshopCollectionURL = "https://steamcommunity.com/sharedfiles/filedetails/?id=" + $Id
+
+  $modList = $Output
+
+  $getPage = Invoke-WebRequest -Uri $WorkshopCollectionURL
+  $modIdCollection = @()
+  $Links = $getPage.Links
+  foreach ($link in $Links) {
+    if ($link.outerHTML -like "*workshopItemTitle*") {
+      $modID = $link.href.Replace('https://steamcommunity.com/sharedfiles/filedetails/?id=','')
+      if($modIDCollection -notcontains $modID) {
+        $desc = $link.outerHTML -replace '\<a\shref=\"https:\/\/steamcommunity.com\/sharedfiles\/filedetails\/\?id\=.*"\>\<div class="workshopItemTitle"\>', ""
+        $desc = $desc.Replace('</div></a>','');
+        if ($modList -ne "echo") {
+          Write-Host "Found Mod: $desc"
+        }
+        $modIDCollection += $modID
+      }
+    }
+  }
+  if ($modList -ne "echo") {
+    if (Test-Path $modList) {
+      del $modList
+    }
+    Set-Content -Path $modList $modIDCollection
+    Write-Host "Your mod list is at: $modList"
+  } else {
+    Write-Host $modIDCollection
+  }
+}
+
+Export-ModuleMember -Function Get-CollectionIds
